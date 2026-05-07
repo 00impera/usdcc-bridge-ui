8ca84a750b7fe736d6b551df57427460c7a0f995
00impera <you@example.com>
2026-05-07 07:59:22 +0200

    fix: add ThirdwebProvider back to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ThirdwebProvider } from 'thirdweb/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <ThirdwebProvider>
+      <App />
+    </ThirdwebProvider>
+  </QueryClientProvider>
 )
